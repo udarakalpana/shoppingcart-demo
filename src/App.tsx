f2c11732f202b985ba8692e166ee17a74aea8fcf
kalpana <kalpanaudara058@gmail.com>
2025-08-20 21:08:49 +0530

    Created navigation bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {Route, Routes} from "react-router";
 import ProductList from "./components/product/ProductList.tsx";
+import NavBar from "./components/nav-bar/NavBar.tsx";
 
 const App = () => {
 
   return (
          <div className="min-h-screen">
+           <NavBar />
            <Routes>
              <Route path="/" element={<ProductList />} />
            </Routes>
